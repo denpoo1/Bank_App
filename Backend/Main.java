cd9c811f2d7aea7ae7cdcdd553dd026c224d66e4
Denys Durbalov <116250966+denpoo1@users.noreply.github.com>
2023-07-13 23:04:22 +0200

    Update Main.java

--- Backend/Main.java
+++ Backend/Main.java
@@ -3,5 +3,6 @@ public class Main {
         System.out.println("Hello world");
         System.out.println("Hello Denis");
         System.out.println("Hello task3");
+        //ded ded ded
     }
 }
