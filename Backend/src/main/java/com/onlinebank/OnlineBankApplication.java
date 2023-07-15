f88401e03716ded21c58bcf06507ee0038072470
Denis Durbalov <den.dyrbalov25@gmail.com>
2023-07-15 18:00:22 +0200

    add backend structure

---
+++ Backend/src/main/java/com/onlinebank/OnlineBankApplication.java
@@ -0,0 +1,13 @@
+package com.onlinebank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineBankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineBankApplication.class, args);
+    }
+
+}
