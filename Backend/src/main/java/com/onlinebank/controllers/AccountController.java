ab949d05c53c5e5fc2d5bf36fe7065f4a90e0444
Denis Durbalov <den.dyrbalov25@gmail.com>
2023-07-20 21:47:26 +0200

    OB-31-Account-controller-with-response-request-exception-handler

---
+++ Backend/src/main/java/com/onlinebank/controllers/AccountController.java
@@ -0,0 +1,94 @@
+package com.onlinebank.controllers;
+
+import com.onlinebank.dto.AccountRequest;
+import com.onlinebank.dto.AccountResponse;
+import com.onlinebank.dto.TransactionResponse;
+import com.onlinebank.models.Account;
+import com.onlinebank.models.Customer;
+import com.onlinebank.models.PiggyBank;
+import com.onlinebank.models.Transaction;
+import com.onlinebank.services.AccountService;
+import com.onlinebank.services.CreditCardService;
+import com.onlinebank.services.CustomerService;
+import com.onlinebank.services.TransactionService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/accounts")
+public class AccountController {
+
+    private final AccountService accountService;
+    private final CustomerService customerService;
+    private final TransactionService transactionService;
+
+    @Autowired
+    public AccountController(AccountService accountService, CustomerService customerService, TransactionService transactionService) {
+        this.accountService = accountService;
+        this.customerService = customerService;
+        this.transactionService = transactionService;
+    }
+
+    @GetMapping
+    public List<AccountResponse> getAccounts() {
+        List<Account> accounts = accountService.getAllAccounts();
+        List<AccountResponse> accountResponses = new ArrayList<>();
+        for (Account account : accounts) {
+            accountResponses.add(new AccountResponse(account));
+        }
+        return accountResponses;
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Object> getAccount(@PathVariable("id") int id) {
+        Account account = accountService.getAccountById(id);
+        if (account != null) {
+            return ResponseEntity.ok(new AccountResponse(account));
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Account with id " + id + " not found");
+        }
+    }
+
+    @PostMapping
+    public ResponseEntity<Object> createAccount(@RequestBody @Valid AccountRequest accountRequest, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            List<FieldError> fieldErrors = bindingResult.getFieldErrors();
+            List<String> errors = new ArrayList<>();
+            for (FieldError fieldError : fieldErrors) {
+                errors.add(fieldError.getDefaultMessage());
+            }
+            return ResponseEntity.badRequest().body(errors);
+        }
+        Customer customer = customerService.getCustomerById(accountRequest.getCustomerId());
+        if (customer == null)
+            return ResponseEntity.badRequest().body("Customer with id " + accountRequest.getCustomerId() + " don't found");
+        Account account = accountRequest.toAccount(customer);
+        account.setPiggyBank(new PiggyBank(account, accountRequest.getDate(), 0));
+        accountService.saveAccount(account);
+        return ResponseEntity.status(HttpStatus.CREATED).body(new AccountResponse(account));
+    }
+
+    @GetMapping("/{id}/transaction")
+    public ResponseEntity<Object> getAccountTransactions(@PathVariable("id") int accountId) {
+        Account account = accountService.getAccountById(accountId);
+        if (account == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Account with id " + accountId + " not found");
+        }
+
+        List<Transaction> transactions = transactionService.getAccountTransactions(accountId);
+        List<TransactionResponse> transactionResponses = new ArrayList<>();
+        for (Transaction transaction : transactions) {
+            transactionResponses.add(new TransactionResponse(transaction));
+        }
+
+        return ResponseEntity.ok(transactionResponses);
+    }
+}
--- Backend/src/main/java/com/onlinebank/services/TransactionService.java
+++ Backend/src/main/java/com/onlinebank/services/TransactionService.java
@@ -7,6 +7,7 @@ import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -50,5 +51,12 @@ public class TransactionService {
         } else {
             throw new EntityNotFoundException("Transaction with ID " + updatedTransaction.getId() + " not found.");
         }
+    }
+
+    public List<Transaction> getAccountTransactions(int accountId) {
+        List<Transaction> transactions = new ArrayList<>();
+        transactions.addAll(transactionRepository.findAllByToAccountId(accountId));
+        transactions.addAll(transactionRepository.findAllByFromAccountId(accountId));
+        return transactions;
     }
 }
