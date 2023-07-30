a308e90c30549558e597264abc38b2a2d13275f7
Denis Durbalov <den.dyrbalov25@gmail.com>
2023-07-30 22:45:34 +0200

    Backend container docker version  2

--- Backend/src/main/java/com/onlinebank/OnlineBankApplication.java
+++ Backend/src/main/java/com/onlinebank/OnlineBankApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
  * @author Denis Durbalov
@@ -13,8 +15,20 @@ import org.springframework.web.client.RestTemplate;
 @SpringBootApplication
 @OpenAPIDefinition
 public class OnlineBankApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(OnlineBankApplication.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer webMvcConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowedHeaders("*");
+            }
+        };
     }
 }
